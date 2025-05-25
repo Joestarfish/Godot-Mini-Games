408393ae017fe7809eeade7a34cf2e33200c22b2
Joestarfish <joestarfish2@gmail.com>
2025-05-25 09:44:11 -0400

    Add hints and a timer before starting next round

--- scripts/random_number_game.gd
+++ scripts/random_number_game.gd
@@ -23,6 +23,9 @@ var tries: int:
 @onready var tries_label: Label = %Tries
 @onready var spin_box: SpinBox = %SpinBox
 @onready var hints_v_box_container := %HintsVBoxContainer
+@onready var next_round_timer: Timer = %NextRoundTimer
+@onready var progress_bar: ProgressBar = %ProgressBar
+@onready var submit_button: Button = %SubmitButton
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -33,7 +36,20 @@ func _ready() -> void:
 	spin_box.max_value = MAX_NUMBER
 	start_next_round()
 
+func _process(delta: float) -> void:
+	progress_bar.value = 100 - (next_round_timer.wait_time - next_round_timer.time_left) / next_round_timer.wait_time * 100
+
+# This function wait for a timer to end before starting the next round. It allows the user to view the answer before we clear it.
+func queue_next_round() -> void:
+	next_round_timer.start()
+	progress_bar.visible = true
+	submit_button.disabled = true
+	spin_box.editable = false
+
 func start_next_round() -> void:
+	progress_bar.visible = false
+	submit_button.disabled = false
+	spin_box.editable = true
 	hints_v_box_container.clear_nodes()
 	spin_box.value = 0
 	tries = max_tries
@@ -42,18 +58,25 @@ func start_next_round() -> void:
 
 func _on_submit_button_pressed() -> void:
 	var input = int(spin_box.value)
+	spin_box.value = 0
 
 	if input == 0:
 		return
 	
 	if input == random_number:
 		score += 1
-		start_next_round()
+		hints_v_box_container.add_hint("The answer was %d\nGood job!" % random_number)
+		queue_next_round()
 		return
 
 	tries -= 1
 	if tries <= 0:
-		start_next_round()
+		hints_v_box_container.add_hint("The answer was %d" % random_number)
+		queue_next_round()
 		return
 
 	hints_v_box_container.add_hint("%s than %d" % ["Lower" if random_number - input < 0 else "Higher", input])
+
+
+func _on_next_round_timer_timeout() -> void:
+	start_next_round()
